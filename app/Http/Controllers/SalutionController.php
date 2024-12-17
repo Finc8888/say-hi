65d49849d5c33e9d8fc8a72367ab4543cb4e6442
friedfox <finc8888@gmail.com>
2024-12-18 00:36:05 +0300

    Saving Salutions(write store method in controller)

--- app/Http/Controllers/SalutionController.php
+++ app/Http/Controllers/SalutionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Salution;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -30,9 +31,15 @@ class SalutionController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->salutions()->create($validated);
+
+        return redirect(route('salutions.index'));
     }
 
     /**
